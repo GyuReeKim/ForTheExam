85e24b17cd76652450c408b73a040a2d37c0f3b2
GyuReeKim <starkim96@naver.com>
2019-10-22 19:40:05 +0900

    after 6pm 1022

---
+++ accounts/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+# django.contrib.auth에서 forms에 AuthenticationForm이 있다.
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+
+# Create your views here.
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # 로그인을 바로 하도록 만들려면 auth_login을 사용하고, redirect를 사용하는 것이 좋다.
+            return redirect('accounts:login')
+    else:
+        form = UserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/form.html', context)
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            # request는 익명의 data를 갖고있기 때문에 form에서 user정보를 가져와야 한다.
+            # get_user()는 함수
+            auth_login(request, form.get_user())
+            return redirect('accounts:base')
+    else:
+        form = AuthenticationForm()
+    context ={
+        'form': form,
+    }
+    return render(request, 'accounts/form.html', context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect('accounts:login')
+
+def base(request):
+    return render(request, 'accounts/base.html')
